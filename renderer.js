6d09138495a3b795c283743254f34ed5bb972420
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-04-24 03:53:39 -0400

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@ export default new class Unthemeable {
 	start() {
 		const style = document.createElement("link")
         style.rel = "stylesheet"
-        style.href = "https://discord-theme.pages.dev/frcat.css"
+        style.href = "https://discord-theme.pages.dev/unthemeable.css"
         document.head.appendChild(style)
 	}
 }
